1e155c1de3320392cd5c8c3935ed1ba7f5ae5509
maksim715 <maksim1708kuznetsow@gmail.com>
2026-06-12 15:25:50 +0300

    fix(backend): Реализовать API для добавления списков - GET /api/dictionaries/directions - список направлений - GET /api/dictionaries/study-formats - форматы обучения - GET /api/dictionaries/statuses - список статусов

---
+++ src/backend/api/Models/DTOs/DictionaryDto.cs
@@ -0,0 +1,9 @@
+namespace api.Models.DTOs
+{
+    // DTO для справочников
+    public class DictionaryDto
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+    }
+}
