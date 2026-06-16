a42126bbffe806e87ad759847803047991be103d
maksim715 <maksim1708kuznetsow@gmail.com>
2026-06-16 18:33:34 +0300

    fix(backend): Переименовать study-formats в training-formats, объединить DictionaryResponseDto, DictionaryDto и DictionaryRequestDto в одном файле, изменить string Id на Guid Id в некоторых местах

--- src/backend/api/Models/DTOs/DictionaryDto.cs
+++ src/backend/api/Models/DTOs/DictionaryDto.cs
@@ -1,8 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api.Models.DTOs
 {
     public class DictionaryDto
     {
         public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+    }
+    
+    public class DictionaryResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = "";
+    }
+
+    public class DictionaryRequestDto
+    {
+        [Required(ErrorMessage = "Название обязательно")]
+        [MinLength(2, ErrorMessage = "Название должно содержать минимум 2 символа")]
+        [MaxLength(100, ErrorMessage = "Название не должно превышать 100 символов")]
         public string Name { get; set; } = "";
     }
 }
--- src/backend/api/Models/DTOs/DictionaryResponseDto.cs
+++
@@ -1,8 +0,0 @@
-namespace api.Models.DTOs
-{
-    public class DictionaryResponseDto
-    {
-        public string Id { get; set; } = "";
-        public string Name { get; set; } = "";
-    }
-}
